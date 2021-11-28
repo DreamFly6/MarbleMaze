f95350d003ec30dee82d265822ef9304c7bce234
NickSagan <9952728@gmail.com>
2021-11-28 17:05:52 +0300

    start func loadLevel() add enum CollisionTypes

---
+++ MarbleMaze/CollisionTypes.swift
@@ -0,0 +1,16 @@
+//
+//  CollisionTypes.swift
+//  MarbleMaze
+//
+//  Created by Nick Sagan on 28.11.2021.
+//
+
+import Foundation
+
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
--- MarbleMaze/GameScene.swift
+++ MarbleMaze/GameScene.swift
@@ -8,9 +8,46 @@
 import SpriteKit
 
 class GameScene: SKScene {
-
+    
     override func didMove(to view: SKView) {
         
+    }
+    
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle.")
+        }
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Could not load level1.txt from the app bundle.")
+        }
+
+        let lines = levelString.components(separatedBy: "\n")
+
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+
+                if letter == "x" {
+                    // load wall
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic = false
+                    addChild(node)
+                } else if letter == "v"  {
+                    // load vortex
+                } else if letter == "s"  {
+                    // load star
+                } else if letter == "f"  {
+                    // load finish
+                } else if letter == " " {
+                    // this is an empty space – do nothing!
+                } else {
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
     }
     
 }
